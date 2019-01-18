3a16a1f80b692157e876c8c949e1d9ec36d743e6
Hugo J. Bello <hjbello.wk@gmail.com>
2019-01-18 14:01:27 +0100

    initial commit

--- index.js
+++ index.js
@@ -19,11 +19,33 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
     bot.sendMessage(chatId, resp);
 });
 
+// Matches "/echo [whatever]"
+bot.onText(/\/info (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, 'Acerca de que quieres que te informe', {
+            reply_markup: {
+                keyboard: [['Programa'], ['Cotilleos']]
+            }
+        }
+    );
+});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/nueva_propuesta_para_programa (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, 'ok te escucho');
+
+});
+
 // Listen for any kind of message. There are different kinds of
 // messages.
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
 
     // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
+    //bot.sendMessage(chatId, 'Received your message');
 });
